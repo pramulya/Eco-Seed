e049c736388dc8a4eadea3c645a7b086fea27ebb
Pramulya <rajamulyapratama4@gmail.com>
2025-05-30 02:08:45 +0700

    Hapus semua isi Raja_Branch sebelum reset dari main

--- resources/views/campaign/show.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="mt-3">
-    <a href="{{ route('campaign.volunteer', $campaign) }}" class="btn btn-primary">Join as Volunteer</a>
-    <a href="{{ route('campaign.volunteers', $campaign) }}" class="btn btn-info">View Volunteers</a>
-</div>
